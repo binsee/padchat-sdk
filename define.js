2174b48ddbc2c8c4dc8a83701bf3119bc1f408ae
binsee <binsee@163.com>
2018-04-04 04:02:30 +0800

    refactor: 补充联系人黑名单

--- define.js
+++ define.js
@@ -20,21 +20,43 @@ exports.userEvents = [
 ]
 
 exports.loginType = {
-  token   : 'token',     // 断线重连
-  request : 'request',   // 二次登陆
-  qrcode  : 'qrcode',    // 扫码登陆
-  phone   : 'phone',     // 手机验证码登陆
-  user    : 'user',      // 账号密码登陆
+  token  : 'token',     // 断线重连
+  request: 'request',   // 二次登陆
+  qrcode : 'qrcode',    // 扫码登陆
+  phone  : 'phone',     // 手机验证码登陆
+  user   : 'user',      // 账号密码登陆
 }
 
 exports.blacklist = [
-  'weixin', // 腾讯团队
-  'newsapp', // 腾讯新闻
-  'tmessage', //
-  'fmessage', // 朋友推荐
-  'qmessage', // qq离线消息
+  'appbrandcustomerservicemsg',
+  'blogapp',
+  'cardpackage',
+  'facebookapp',
+  'feedsapp',
+  'filehelper',
+  'floatbottle',
   'floatbottle', // 漂流瓶
-  'medianote', // 语音记事本
+  'fmessage', // 朋友推荐
+  'helper_entry',
+  'lbsapp',
+  'linkedinplugin',
+  'masssendapp',
+  'medianote',   // 语音记事本
   'mphelper', // 公众平台安全助手
+  'newsapp', // 腾讯新闻
+  'notification_messages',
+  'notifymessage',
+  'officialaccounts',
+  'pc_share',
+  'qmessage', // qq离线消息
+  'qqfriend',
+  'qqmail',
+  'qqsync',
+  'shakeapp',
+  'tmessage',
+  'voiceinputapp',
+  'voicevoipapp',
+  'voipapp',
   'weibo', // 微博-未知
+  'weixin', // 腾讯团队
 ]
